beed585cb63ff2a05961ecd2ce61b13fd67b120b
Marius van der Busse <mariusvanderbusse@hotmail.com>
2020-01-06 18:58:14 +0200

    Updates

---
+++ precache-manifest.24382ebcf40423ec29da350bcaa4ae6f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0abe7b66f7261edce80a023046c242",
+    "url": "/fitnessScheduler/index.html"
+  },
+  {
+    "revision": "a0075cfd4195ec9b0e6a",
+    "url": "/fitnessScheduler/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "f001e7acb33e1369fd90",
+    "url": "/fitnessScheduler/static/css/main.39745ba6.chunk.css"
+  },
+  {
+    "revision": "a0075cfd4195ec9b0e6a",
+    "url": "/fitnessScheduler/static/js/2.d1570aac.chunk.js"
+  },
+  {
+    "revision": "f001e7acb33e1369fd90",
+    "url": "/fitnessScheduler/static/js/main.0ed37362.chunk.js"
+  },
+  {
+    "revision": "9f22ab6882cd3ad8fb15",
+    "url": "/fitnessScheduler/static/js/runtime-main.529fe0f0.js"
+  },
+  {
+    "revision": "fed3e4a5bb60e289aae4c14ab56b3cd3",
+    "url": "/fitnessScheduler/static/media/0.fed3e4a5.jpg"
+  },
+  {
+    "revision": "7652dffc574cc0d1a0be4ddfa03d6e27",
+    "url": "/fitnessScheduler/static/media/1.7652dffc.jpg"
+  },
+  {
+    "revision": "b5e0701abbc21a798e50188d9176bb91",
+    "url": "/fitnessScheduler/static/media/2.b5e0701a.jpg"
+  },
+  {
+    "revision": "7ddb70ccf8e545b95f0525591c025513",
+    "url": "/fitnessScheduler/static/media/3.7ddb70cc.jpg"
+  },
+  {
+    "revision": "7f580de2075b311ae7cafd3c33fc299d",
+    "url": "/fitnessScheduler/static/media/4.7f580de2.jpg"
+  },
+  {
+    "revision": "a2d5122997297ce0e32e04b4dd0d29b7",
+    "url": "/fitnessScheduler/static/media/5.a2d51229.jpg"
+  },
+  {
+    "revision": "7a00faa6897d5d055ea5b6355e0fbe20",
+    "url": "/fitnessScheduler/static/media/6.7a00faa6.jpg"
+  },
+  {
+    "revision": "4354ac30273e3175636bb0c9e5a2a53b",
+    "url": "/fitnessScheduler/static/media/7.4354ac30.jpg"
+  },
+  {
+    "revision": "449234076f30b14dc2a099002da564f4",
+    "url": "/fitnessScheduler/static/media/8.44923407.jpg"
+  },
+  {
+    "revision": "21781198614bf20a8078f38ea9a191d9",
+    "url": "/fitnessScheduler/static/media/Fitness.21781198.jpeg"
+  },
+  {
+    "revision": "66c18287b1de11008397cbd7cd3e8235",
+    "url": "/fitnessScheduler/static/media/Gym training.66c18287.jpeg"
+  },
+  {
+    "revision": "be19579e62c5b06289d7159a9bf18a7a",
+    "url": "/fitnessScheduler/static/media/Jogging.be19579e.jpeg"
+  },
+  {
+    "revision": "24a64d5a98267eb431b940e83c9f38c5",
+    "url": "/fitnessScheduler/static/media/Spinning.24a64d5a.jpeg"
+  },
+  {
+    "revision": "b1d2e430a39decd8c661b8de5f106bc5",
+    "url": "/fitnessScheduler/static/media/Zumba.b1d2e430.jpeg"
+  }
+]);
